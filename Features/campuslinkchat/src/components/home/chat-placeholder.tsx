945b503b345ecb581316ad8b9cd2dd0e9f9f0d49
LazarusUno <alazarashebir7@gmail.com>
2024-10-03 06:40:48 +0300

    added features

---
+++ Features/campuslinkchat/src/components/home/chat-placeholder.tsx
@@ -0,0 +1,15 @@
+import { Lock } from "lucide-react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+const ChatPlaceHolder = () => {
+	return (
+		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
+			
+			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1'>
+				<Lock size={10} /> Your personal messages are end-to-end encrypted
+			</p>
+		</div>
+	);
+};
+export default ChatPlaceHolder;
