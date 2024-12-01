deb8b7100506c5d79aaf2210585c7983a78661ed
Estifanos Behailu <128400498+Estifanos-Behailu@users.noreply.github.com>
2024-12-01 14:54:41 +0300

    Student page problem
    For the problem of seeing each students profile

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function adjustScheduleToCurrentWeek(data) {
+  const today = new Date();
+  const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 1));
+  const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7));
+
+  return data.filter(event => {
+    const eventStart = new Date(event.start);
+    return eventStart >= startOfWeek && eventStart <= endOfWeek;
+  });
+}
